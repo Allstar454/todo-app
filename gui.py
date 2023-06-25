2a9c449a850010cd6139621884cb30ada5d9d06f
Allstar454 <yephyowin@icloud.com>
2023-06-25 13:24:10 +0800

    Add Edit Button

--- gui.py
+++ gui.py
@@ -1,25 +1,57 @@
-import functions
+import functions as fun
 import PySimpleGUI as sg
 
+#Add
+label_add = sg.Text("Type in a To-Do")
 
-input_label = sg.Text("Type in a to-do")
-input_box1 = sg.InputText(tooltip="Enter to do", key="input1")
-box1_button = sg.Button("Add")
+inputbox_add = sg.InputText(tooltip="Enter to do", key="add_key")
+button_add = sg.Button("Add")
+
+#Edit
+listbox_edit = sg.Listbox(values=fun.get_todos(), key="edit_key",
+                          enable_events=True, size=(45, 10))
+button_edit = sg.Button("Edit")
 
 window = sg.Window("My To-Do App",
-                   layout=[[input_label], [input_box1, box1_button]],
-                   font=("Helvetica", 15))
+                   layout=[[label_add], [inputbox_add, button_add],
+                   [listbox_edit, button_edit]],
+                   font=("Helvetica", 12))
 
 while True:
-    action, result = window.read()
+    action, input_data = window.read()
     print(action)
-    print(result)
+    print(input_data)
 
     match action:
+
         case "Add":
-            todos = functions.get_todos()
-            todos.append(result["input1"]+'\n')
-            functions.write_todos("w", todos)
+            todos_list = fun.get_todos()
+            if input_data["add_key"].strip():
+                todos_list.append(input_data["add_key"]+'\n')
+                fun.write_todos("w", todos_list)
+                window["edit_key"].update(values=todos_list)
+                window["add_key"].update(value="")
+
+        case "Edit":
+            todos_list = fun.get_todos()
+            index_of_edit = ""
+
+            if input_data["edit_key"]:
+                todo_to_edit = input_data["edit_key"][0]
+                if todo_to_edit:
+                    index_of_edit = todos_list.index(todo_to_edit)
+
+            new_todo = input_data["add_key"].strip()
+
+            if new_todo and index_of_edit >= 0:
+                todos_list[index_of_edit] = new_todo+'\n'
+                fun.write_todos("w", todos_list)
+                window["edit_key"].update(values=todos_list)
+                window["add_key"].update(value="")
+
+        case "edit_key":
+            window["add_key"].update(value=input_data['edit_key'][0].strip())
+
         case sg.WIN_CLOSED:
             break
 
